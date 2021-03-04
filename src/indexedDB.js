cb488fcb4c0de341f84c746310191e5aeafc63ec
DerekAlmanza <Derek.Almanza@ciencias.unam.mx>
2021-03-03 22:02:14 -0600

    Escribir y leer elementos de la BD de IndexedBD.

--- src/indexedDB.js
+++ src/indexedDB.js
@@ -12,23 +12,74 @@ Métodos: * open(): Crear la base de datos.
 */
 
 const indexedDB = window.indexedDB;
+const form = document.getElementById('form');
+const tareasLista = document.getElementById('tareasLista');
 
-if (indexedDB) {
+if (indexedDB && form) {
     let baseDatos;
     const respuesta = indexedDB.open('listaTareas', 1);
 
     respuesta.onsuccess = () => {
         baseDatos = respuesta.result;
         console.log('OPEN', baseDatos);
+        leerInfo(); // Porque es asíncrono, y para leer necesitamo que la BD ya esté abierta.
     }
 
     respuesta.onupgradeneeded = () => {
         baseDatos = respuesta.result;
         console.log('CREATE', baseDatos);
-        const objectStore = baseDatos.createObjectStore('tareas');
+        const objectStore = baseDatos.createObjectStore('tareas', {
+            // autoIncrement:true // Te asigna una llave automática
+            keyPath: 'titulo' // Nosotros le asignamos llave
+        });
     }
 
     respuesta.onerror = () => {
         console.log('ERROR', error);
     }
+
+    // Añadir datos 
+    const añadirInfo = (informacionTarea) => {
+        const transaccion = baseDatos.transaction(['tareas'], 'readwrite');
+        const objectStore = transaccion.objectStore('tareas');
+        objectStore.add(informacionTarea);
+        leerInfo();
+    }
+
+    // Leer datos 
+    const leerInfo = () => {
+        const transaccion = baseDatos.transaction(['tareas'], 'readonly');
+        const objectStore = transaccion.objectStore('tareas');
+        const informacion = objectStore.openCursor();
+        const fragmento = document.createDocumentFragment();
+
+        informacion.onsuccess = (evento) => {
+            const cursor = evento.target.result;
+            if(cursor) { // Porque si termina de leer, entonces ya no habrá cursor y se detendrá.
+                const infoTarea = document.createElement('li');
+                const tituloTarea = cursor.value.titulo;
+                const prioridadTarea = cursor.value.prioridad;
+                infoTarea.classList.add('infoTarea');
+                infoTarea.innerHTML = `Título: ${tituloTarea} / Prioridad: ${prioridadTarea} <br><br>`;
+                fragmento.appendChild(infoTarea);
+                // console.log(cursor.value); 
+                cursor.continue();
+            } else {
+                tareasLista.textContent = '';
+                tareasLista.appendChild(fragmento);
+                console.log('No hay ya informacion');
+            }
+        }
+    }
+
+    form.addEventListener('submit', (evento) => {
+        evento.preventDefault();
+        console.log(evento);
+        const informacionTarea = {
+            titulo: evento.target.tarea.value,
+            prioridad: evento.target.prioridad.value
+        }
+        console.log(informacionTarea);
+        añadirInfo(informacionTarea);
+    })
 }
